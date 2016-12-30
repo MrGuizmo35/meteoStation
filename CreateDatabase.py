c60042264339a82a4fae63bffa61030e236eb02c
Guillaume CORU <guillaume@MacBook-Air-de-Guillaume.local>
2016-12-30 17:22:32 +0100

    Now handles a MySql database locally!!

---
+++ CreateDatabase.py
@@ -0,0 +1,7 @@
+#coding: utf8
+
+from peewee import *
+from Meteo import *
+
+Meteo.create_table()
+
---
+++ Meteo.py
@@ -0,0 +1,28 @@
+#coding: utf8
+
+from peewee import *
+
+f = open("../.mySQLCredentials",'r')
+cr = f.readline()
+f.close()
+t = cr.split(',')
+u = t[0]
+p = t[1][:-1]
+
+print(p)
+	
+db = MySQLDatabase('meteoStation',user=u,passwd=p)
+
+class Meteo(Model):
+	dataDateTime = IntegerField()
+	designation = CharField()
+	description = TextField()
+	temperature = FloatField()
+	humidity = IntegerField()
+	windSpeed = FloatField()
+	windDirection = CharField()
+	
+
+	class Meta:
+		database = db
+
