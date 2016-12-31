972e4c26e828b8dc9c9f94014c1fe63cf18e3ff2
Guillaume CORU <mr.guizmo35@gmail.com>
2016-12-31 15:56:29 +0100

    pic12f device to read room temperature

---
+++ PicTempReader.X/main.c
@@ -0,0 +1,41 @@
+/*
+ * File:   main.c
+ * Author: guillaume
+ *
+ * Created on 31 décembre 2016, 15:33
+ */
+// CONFIG1
+#pragma config FOSC = INTOSC    //  (INTOSC oscillator; I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LPBOREN = OFF    // Low Power Brown-out Reset enable bit (LPBOR is disabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+void main(void) {
+    unsigned int i, j;
+    
+    TRISA5 = 0;
+    LATA5 = 0;
+    while(1)
+    {
+        LATA5 = ~LATA5;
+        for(i = 0; i < 255; i++)
+            for(j = 0; j < 255; j++);
+    }
+    return;
+}
